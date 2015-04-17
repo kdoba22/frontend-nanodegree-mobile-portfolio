8b9ca883a3a0e7689ddea17707db29f4b835df34
Keith Doba <kdoba22@gmail.com>
2015-04-16 20:38:08 -0500

    one more time

--- views/js/main.js
+++ views/js/main.js
@@ -518,11 +518,11 @@ function updatePositions() {
    items[i].style.left = items[i].basicLeft + 100*phase + 'px';
   }
 
-  for (var i = 0; i < len; i++) {
-//    var phase = Math.sin(scrollNumber + (i % 5));
-    var phase = phaseArray[i % 5];
-    items[i].style.transform = 'translateX(' + (phase) + 'px)';
-  }
+//   for (var i = 0; i < len; i++) {
+// //    var phase = Math.sin(scrollNumber + (i % 5));
+//     var phase = phaseArray[i % 5];
+//     items[i].style.transform = 'translateX(' + (phase) + 'px)';
+//   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
